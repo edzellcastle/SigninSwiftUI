a5989b4e2d904aaf4f70102735677eaccfd007ba
David Lindsay <david_lee_lindsay@yahoo.com>
2021-10-15 20:00:38 -0700

    Initial Commit

---
+++ SigninSwiftUI/SigninSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SigninSwiftUIApp.swift
+//  SigninSwiftUI
+//
+//  Created by David Lindsay on 10/15/21.
+//
+
+import SwiftUI
+
+@main
+struct SigninSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
