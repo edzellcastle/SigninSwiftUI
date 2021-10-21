7420bda42ec2c03dd71364a6f61a3e40400e2cd9
David Lindsay <david_lee_lindsay@yahoo.com>
2021-10-21 10:03:22 -0700

    Add a UITest.

--- SigninSwiftUIUITests/SigninSwiftUIUITests.swift
+++ SigninSwiftUIUITests/SigninSwiftUIUITests.swift
@@ -30,6 +30,25 @@ class SigninSwiftUIUITests: XCTestCase {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+    
+    func testProfileCreationLabel() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        let title = app.staticTexts["Profile Creation"]
+        let subTitle = app.staticTexts["Use the form below to submit your porfolio.\nAn email and password are required."]
+        let field1 = app.staticTexts["First Name"]
+        let field2 = app.staticTexts["Email Address"]
+        let field3 = app.staticTexts["Password"]
+        let field4 = app.staticTexts["Website"]
+        
+        XCTAssert(title.exists)
+        XCTAssert(subTitle.exists)
+        XCTAssert(field1.exists)
+        XCTAssert(field2.exists)
+        XCTAssert(field3.exists)
+        XCTAssert(field4.exists)
+    }
 
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
